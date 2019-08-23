d1d7a44fae4445eed215d01c044a72c3539e84b7
cx1964 <cx1964@mail.com>
2019-08-23 15:04:12 +0200

    Aanpassing chordify_musescore_bvw1048_music21_py3.py tbv harmonische analyse

--- chordify_musescore_bvw1048_music21_py3.py
+++ chordify_musescore_bvw1048_music21_py3.py
@@ -8,7 +8,8 @@ import music21 as m
 
 padBladmuziek = "~/Documents/sources/python/python3/python3_music21"
 # Export de MuseScore File in musicxml (uncompressed music mxl format)
-museScoreFile = "BWV1048_Brandenburg_Concerto_No_3_in_G_Major_in_parts_orgineel.musicxml"
+museScoreFile = "BWV1048_Brandenburg_Concerto_No_3_in_G_Major_in_parts_orgineel_1st_mov.musicxml"
+keyG = m.key.Key('G') # BWV1048 1 st move is in G major
 
 # Zie: https://web.mit.edu/music21/doc/usersGuide/usersGuide_24_environment.html#usersguide-24-environment
 env = m.environment.UserSettings()
@@ -23,5 +24,15 @@ env['musicxmlPath'] = '/usr/bin/musescore3'
 bladmuziek = m.converter.parse(padBladmuziek+'/'+museScoreFile, format='musicxml')
 bladmuziekMetAccoorden = bladmuziek.chordify()
 # Toon bladmuziek
-bladmuziekMetAccoorden.show()
+#bladmuziekMetAccoorden.show()
 
+# help(bladmuziekMetAccoorden)
+
+# Harmonische Analyse (benoem akkoorden)
+for akkoord in bladmuziekMetAccoorden.recurse().getElementsByClass('Chord'):
+    # print ("Akkoord: " + str(chord))
+    rn = m.roman.romanNumeralFromChord(akkoord, keyG)
+    # print ("rn= " + str(rn))
+    # benoem akkoord obv romeinse cijfers
+    akkoord.addLyric(str(rn.figure))
+bladmuziekMetAccoorden.show()    
